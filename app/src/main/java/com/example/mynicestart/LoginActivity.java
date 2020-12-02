f7830b4ef3de397bfe8bea9f79a5c9ffad4d9e28
jorgeballesterosalonso <jorgitoballes@gmail.com>
2020-12-02 15:57:10 +0100

    Varios cambios de clases acumuladas

--- app/src/main/java/com/example/mynicestart/LoginActivity.java
+++ app/src/main/java/com/example/mynicestart/LoginActivity.java
@@ -2,7 +2,11 @@ package com.example.mynicestart;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button login = findViewById(R.id.btnLogin);
+        TextView signup = findViewById(R.id.dontHaveAnAC);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/mynicestart/SplashScreen.java
@@ -0,0 +1,66 @@
+package com.example.mynicestart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+
+        openApp(true);
+
+        TextView tv = findViewById(R.id.textView);
+        ImageView imagen = findViewById(R.id.imageView4);
+
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        Animation rotation = AnimationUtils.loadAnimation(this, R.anim.rotate_animation);
+
+        tv.startAnimation(myanim);
+        imagen.startAnimation(rotation);
+
+
+        ImageView fondo = findViewById(R.id.imageView3);
+
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1606122644544-35fee06ab48e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=656&q=80")
+                .centerCrop()
+                .transition(DrawableTransitionOptions.withCrossFade(500))
+                //.placeholder(new ColorDrawable(this.getResources()
+                //.getColor(R.color.design_default_color_background)))
+                //.circleCrop()
+                //.getDiskCacheStrategy(DiskCacheStrategy.NONE)
+                .into(fondo);
+    }
+
+
+
+
+    private void openApp(boolean locationPermission) {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen
+                        .this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+    }
+}
